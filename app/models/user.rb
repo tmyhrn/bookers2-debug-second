e134863b59b8c01b4bb082af0c097e0ae5eb818e
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-10 17:01:31 +0900

    [InTheMiddle]応用課題4

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,35 @@ class User < ApplicationRecord
   has_many :books
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  
+  # フォローをした、されたの関係
+  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  
+  # 一覧画面で使う
+  has_many :following_users, through: :followers, source: :followed
+  has_many :follower_users, through: :followeds, source: :follower
+  
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
 
   def get_profile_image
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
+  end
+  
+  #  フォローしたときの処理
+  def follow(user_id)
+    followers.create(followed_id: user_id)
+  end
+
+  #　フォローを外すときの処理
+  def unfollow(user_id)
+    followers.find_by(followed_id: user_id).destroy
+  end
+
+  #  フォローしていればtrueを返す
+  def following?(user)
+    following_users.include?(user)
   end
 end
