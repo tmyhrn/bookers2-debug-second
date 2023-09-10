53e2cebbdc102f9d78cf000a1bcf4090c7301192
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-10 17:41:54 +0900

    [Finish]応用課題5終了

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,9 @@
 Rails.application.routes.draw do
-  get 'relationships/create'
-  get 'relationships/destroy'
-  get 'book_comments/create'
-  get 'book_comments/destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
-
+  get "search" => "searches#search"
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
