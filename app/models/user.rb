f85b12aa225a53ea3397297bef570647e2233a36
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-10 17:20:39 +0900

    [Finish]応用課題4終了

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,13 +8,11 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   
-  # フォローをした、されたの関係
-  has_many :followers, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :followeds, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  
-  # 一覧画面で使う
-  has_many :following_users, through: :followers, source: :followed
-  has_many :follower_users, through: :followeds, source: :follower
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
   
   has_one_attached :profile_image
 
@@ -26,16 +24,14 @@ class User < ApplicationRecord
   
   #  フォローしたときの処理
   def follow(user_id)
-    followers.create(followed_id: user_id)
+    relationships.create(followed_id: user_id)
   end
-
-  #　フォローを外すときの処理
+  # フォローを外すときの処理
   def unfollow(user_id)
-    followers.find_by(followed_id: user_id).destroy
+    relationships.find_by(followed_id: user_id).destroy
   end
-
-  #  フォローしていればtrueを返す
+  # フォローしているか判定
   def following?(user)
-    following_users.include?(user)
+    followings.include?(user)
   end
 end
