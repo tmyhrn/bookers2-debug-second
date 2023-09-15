59a3ae770c0bc48ecc1dd8f4a6a05522bbe90ea2
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-15 18:32:45 +0900

    [Finish]応用課題9a終了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,10 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @newbook = Book.new
     @book_comment = BookComment.new
+    @book_detail = Book.find(params[:id])
+    unless ReadCount.find_by(user_id: current_user.id, book_id: @book_detail.id)
+      current_user.read_counts.create(book_id: @book_detail.id)
+    end
   end
 
   def index
