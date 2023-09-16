c8a5cf8eedacb51bb220546fc80312421c028722
Hirano Tomoya <ht.tomoya0605@gmail.com>
2023-09-16 12:02:41 +0900

    [InTheMiddle]応用課題9d途中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -49,6 +49,6 @@ class BooksController < ApplicationController
   private
 
   def book_params
-    params.require(:book).permit(:title, :body, :image, :star)
+    params.require(:book).permit(:title, :body, :image, :star, :category)
   end
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -6,6 +6,7 @@ class Book < ApplicationRecord
   
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
+  validates :category, presence: true
   
   # 投稿数の表示
   scope :created_today, -> { where(created_at: Time.zone.now.all_day) }
@@ -18,7 +19,7 @@ class Book < ApplicationRecord
   scope :created_this_week, -> { where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day) } 
   scope :created_last_week, -> { where(created_at: 2.week.ago.beginning_of_day..1.week.ago.end_of_day) } 
   
-  # 「新しい順」「古い順」「評価の高い順」で
+  # 「新しい順」「古い順」「評価の高い順」でソート
   scope :latest, -> {order(created_at: :desc)}
   scope :old, -> {order(created_at: :asc)}
   scope :star_count, -> {order(star: :desc)}
